947fba41125cc91ec8844160c99e662d0b4f0020
Hilab <>
2019-02-13 13:19:22 +0900

    fix

--- Assets/Plugins/SharedMemory.cs
+++ Assets/Plugins/SharedMemory.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class SharedMemory : MonoBehaviour
 {
@@ -13,29 +13,63 @@ public class SharedMemory : MonoBehaviour
     [DllImport ("SharedMemoryForUnity")]
     private static extern  void setupSharedMemory(string name, int size);
     
+    [DllImport ("SharedMemoryForUnity")]
+    private static extern  void setupSharedMemory_img(string name, int size);
+    
    [DllImport ("SharedMemoryForUnity")]
    private static extern IntPtr getMsg();
 
+   [DllImport ("SharedMemoryForUnity")]
+   private static extern IntPtr getImg();
+
     private IntPtr msgPtr = IntPtr.Zero;
+    private IntPtr imgPtr = IntPtr.Zero;
 
     public byte[] memory;
+    public byte[] memory_img;
 
     public string sharedMsg;
 
-    // Start is called before the first frame update
+    public int size;
+    public int size_img;
+
+    public Texture2D tex;
+    
+    public RawImage RImg;
+    public Text text;
+    public Text text_fps;
+    
     void Start()
     {
-        setupSharedMemory("0",1024);
-        memory = new byte[1024];
+        size = 1024;
+        size_img = 640*480*3;
+        
+        setupSharedMemory("0",size);
+        setupSharedMemory_img("1",size_img);
+        
+        memory = new byte[size];
+        memory_img = new byte[size_img];
+        
+        tex = new Texture2D(640, 480, TextureFormat.RGB24, false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        msgPtr = getMsg();
+        float fps = 1f / Time.deltaTime;
         
-        Marshal.Copy(msgPtr, memory, 0, 1024);
+        msgPtr = getMsg();
+        imgPtr = getImg();
+        
+        Marshal.Copy(msgPtr, memory, 0, size);
+        Marshal.Copy(imgPtr, memory_img, 0, size_img);
         
         sharedMsg = System.Text.Encoding.UTF8.GetString(memory);
+        text.text = sharedMsg;
+        
+        tex.LoadRawTextureData(memory_img);
+        tex.Apply();
+        RImg.texture = tex;
+         
+        text_fps.text = fps.ToString();
     }
 }
--- mac-plugin/SharedMemoryForUnity/SharedMemoryForUnity/Plugin.cpp
+++ mac-plugin/SharedMemoryForUnity/SharedMemoryForUnity/Plugin.cpp
@@ -10,15 +10,28 @@
 #include "Poco/SharedMemory.h"
 
 static Poco::SharedMemory client;
+static Poco::SharedMemory client_img;
 static int bufferSize;
+static int bufferSize_img;
 
 void setupSharedMemory(char* name, int size){
     bufferSize = size;
     client = Poco::SharedMemory(name, bufferSize, Poco::SharedMemory::AccessMode::AM_READ, 0, false);
 }
 
+void setupSharedMemory_img(char* name, int size){
+    bufferSize_img = size;
+    client_img = Poco::SharedMemory(name, bufferSize_img, Poco::SharedMemory::AccessMode::AM_READ, 0, false);
+}
+
 char* getMsg(){
     //memcpy(msg, client.begin(), bufferSize);
     
     return client.begin();
+}
+
+char* getImg(){
+    //memcpy(msg, client.begin(), bufferSize);
+    
+    return client_img.begin();
 }
